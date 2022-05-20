8a5bdb6d208fe56f3bbde479a4c342e1d690ea57
janmejaysinghh <95363621+janmejaysinghh@users.noreply.github.com>
2022-05-20 13:35:56 +0530

    Add files via upload

---
+++ mathematicaloperator.py
@@ -0,0 +1,17 @@
+n1=int(input("Enter the first number"))
+n2=int(input("Enter the second number"))
+operator = input("Enter mathematical operator")
+if operator=='+':
+ print("Sum of first and second number is:",n1+n2)
+if operator=='-':
+ print("Difference of first and second number is:",n1-n2)
+if operator=='*':
+ print("product of first and second number is:",n1*n2)
+if operator=='/':
+ print("Dividend of first and second number is:",n1/n2)
+if operator=='%':
+ print("Moduls of first and second number is:",n1%n2)
+if operator=='//':
+ print("Floor division of first and second number is:",n1//n2)
+if operator=='**':
+ print("Exponent of first and second number is:",n1**nb2)
